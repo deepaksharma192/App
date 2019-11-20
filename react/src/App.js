0cd248c0da41d2361f8bb4b33f606ef1731b4eb1
Deepak Sharma <sharma.deepak192@gmail.com>
2019-11-20 07:26:34 -0800

    v3

---
+++ react/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import Login from './Components/Login';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function App() {
+  const isLogin =true;
+  return (
+    <div className="App">
+      {/* <Header isLogin={isLogin}/> */}
+      <Router>
+      <Route path="/" component={Login} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
